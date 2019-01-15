82f1bb3b654ee67c6e2aab70d674e37e30ca0625
Eclésio Júnior <eclesio@alphasoftware.com.br>
2019-01-15 17:59:51 -0400

    Init test project

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,22 @@
+using Prime.Services;
+using Xunit;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
